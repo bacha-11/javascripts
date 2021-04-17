95d8bca99e0049511a67014c0e0e83c3db528b85
bacha-11 <ijazbacha14@gmail.com>
2021-04-18 01:46:53 +0500

    object inside in object in js

--- oop.js
+++ oop.js
@@ -1,12 +1,27 @@
 
-let student = {
-    stdId : 1,
-    stdName : 'ijaz',
-    stdAge : 24,
-    get_data : function(){
-            console.log(`My Name is ${student.stdName} and Age is ${student.stdAge}.`);
-            }
-}
+// let student = {
+//     stdId : 1,
+//     stdName : 'ijaz',
+//     stdAge : 24,
+//     get_data : function(){
+//             console.log(`My Name is ${student.stdName} and Age is ${student.stdAge}.`);
+//             }
+// }
 
-// console.log(student.stdName)
-student.get_data()
+// // console.log(student.stdName)
+// student.get_data()
+
+let student = {
+        stdId : 1,
+        stdName : {
+            firstName : 'ijaz',
+            lastName : 'bacha',
+        },
+        stdAge : 24,
+        get_data : function(){
+                console.log(`My lastName is ${student.stdName.lastName} and Age is ${student.stdAge}.`);
+                }
+    }
+    
+    //  console.log(student.stdName)
+    student.get_data()
