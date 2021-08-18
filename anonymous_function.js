f715b5d0276886c59e90f11ab32442a92faba6d5
bacha-11 <ijazbacha14@gmail.com>
2021-08-18 17:18:39 +0500

    nest functions

--- anonymous_function.js
+++ anonymous_function.js
@@ -17,6 +17,34 @@ global.onload = function(){console.log('Hi there')}
 setTimeout(() => {console.log("Time to take the cookies out of the oven")}, 2000);
 
 
+// nest functions
+
+var migration = true
+
+var flying = function(num){
+    sound = 'flying';
+    function wingFlapper(){
+        console.log(sound);
+    }
+    for(var i=0; i<num; i++){
+        wingFlapper()
+    }
+}
+
+flying(3)
+
+function quack(num){
+    sound = 'quacker';
+    var quacker = function(){
+        console.log(sound)
+    }
+    for(var i=0; i<num; i++){
+        quacker()
+    }
+}
+
+quack(3)
+
 
 
 
