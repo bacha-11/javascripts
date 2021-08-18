9abad43d4e345a7f671dadfda6dddfbdcc3751c2
bacha-11 <ijazbacha14@gmail.com>
2021-08-18 16:43:07 +0500

    anonymous function

---
+++ anonymous_function.js
@@ -0,0 +1,19 @@
+
+// With simple function declaration
+
+function handler(){
+    console.log('hi there')
+}
+
+global.onload = handler;
+
+
+// Anonymous function
+
+global.onload = function(){console.log('Hi there')}
+
+
+
+
+
+
